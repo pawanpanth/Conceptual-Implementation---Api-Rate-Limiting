d9b39d40696a0f68ab7f519666afb937ec0c0da5
pawan panth <pawan.panth.pp@gmail.com>
2020-12-10 17:28:47 +0530

    m

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const posts = require('./initialData');
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-let count;
+let count = 0;
 const fun = () =>{
     setInterval(()=>{count = 0},30000);
 
@@ -24,19 +24,22 @@ app.use(bodyParser.json())
 // your code goes here
 let startTime =     
 app.get("/api/posts",(req,res)=>{
+    
     if(req.query.max){
-        if(count === 0){       
+        if(count === 0){ 
             increaseCount();
             fun();
             if(req.query.max >= 21){
-                res.send(posts.slice(0,10));
                 firstMax = 10;
+                res.send(posts.slice(0,10));
+                
             }else{
-                res.send(posts.slice(0,req.query.max));
                 firstMax = req.query.max;
+                res.send(posts.slice(0,req.query.max));
+                
             }       
         }
-        if(count >= 4){
+        else if(count >= 4){
             res.status(429).send({message: "Exceed Number of API Calls"});
         }else{
             let maxVal = Math.min(firstMax,req.query.max);
