e3f3aa66001dcf1b9307af914f634e20ab4a1e44
pawan panth <pawan.panth.pp@gmail.com>
2020-12-10 20:28:52 +0530

    m

--- src/index.js
+++ src/index.js
@@ -24,31 +24,49 @@ app.use(bodyParser.json())
 // your code goes here
 let startTime =     
 app.get("/api/posts",(req,res)=>{
-    
-    if(req.query.max){
-        if(count === 0){ 
-            increaseCount();
-            fun();
-            if(req.query.max >= 21){
+    if(count === 0){
+        fun();
+    }
+    increaseCount();
+
+        if(count === 1 ){ 
+            if(!req.query.max){
+                console.log("i am first and i dont have max");
+                res.send(posts.slice(0,10));
+            }
+            else if(req.query.max >= 21){
+                console.log("i am first and my max is greater than 20");
+
                 firstMax = 10;
                 res.send(posts.slice(0,10));
                 
             }else{
+                console.log("i am first and my max is less than 20");
+
                 firstMax = req.query.max;
                 res.send(posts.slice(0,req.query.max));
                 
             }       
         }
-        else if(count >= 4){
+        else if(count >= 5){
             res.status(429).send({message: "Exceed Number of API Calls"});
         }else{
-            let maxVal = Math.min(firstMax,req.query.max);
-            increaseCount();
-            res.send(posts.slice(0,maxVal));
+
+            if(firstMax && req.query.max){
+                let maxVal = Math.min(firstMax,req.query.max);
+                res.send(posts.slice(0,maxVal));
+           }else if(!req.query.max && !firstMax) {
+               res.send(posts.slice(0,10));
+           }else if(!firstMax && req.query.max){
+               firstMax = req.query.max;
+               res.send(posts.slice(0,req.query.max));
+           }else{
+            res.send(posts.slice(0,10));
+           }
+
+
         }
-    }else{
-        res.send(posts.slice(0,10));
-    }
+    
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
